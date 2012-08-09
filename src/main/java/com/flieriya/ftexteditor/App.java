3f8eeb006a84645fa352ea86ed5b02e15d7e6e01
Ulyana Kucheryavaya <flieriya@gmail.com>
2012-08-09 17:35:51 +0300

    add swing window

--- src/main/java/com/flieriya/ftexteditor/App.java
+++ src/main/java/com/flieriya/ftexteditor/App.java
@@ -1,13 +1,25 @@
 package com.flieriya.ftexteditor;
 
+import javax.swing.*;
+
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App extends JFrame {
+    public App(){
+       setTitle("Hello");
+        setSize(300,200);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    }
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+
+            public void run() {
+                App app = new App();
+                app.setVisible(true);
+            }
+        });
+
     }
 }
