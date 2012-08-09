b2870004d73804158f2770bbb6d6b1a0172c8f40
Ulyana Kucheryavaya <flieriya@gmail.com>
2012-08-09 16:37:49 +0300

    add pom

---
+++ src/main/java/com/flieriya/ftexteditor/App.java
@@ -0,0 +1,13 @@
+package com.flieriya.ftexteditor;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
